d1f49ae178db122db1f61e6fbbc31f46e28899a7
Ceres <ddruva.boomer@gmail.com>
2021-01-18 21:44:19 +0530

    test

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -37,7 +37,7 @@ def embed_class(self, record, author: discord.Member):
         typer = "today"
     else:
         typer = 'tomorrow'
-    embed = discord.Embed(title=f"{subject} class {typer}", description=desc, timestamp=dt.now())
+    embed = discord.Embed(title=f"{subject} class {typer}", description=desc, timestamp=dt.utcnow())
     embed.set_footer(text=f'Invoked by {author.name}', icon_url=author.avatar_url)
     return embed
 
