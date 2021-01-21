06ac92c8834a02bae219e68b121e1e97c4343792
Ceres <ddruva.boomer@gmail.com>
2021-01-21 10:36:52 +0530

    tes

--- cogs/reminder.py
+++ cogs/reminder.py
@@ -164,7 +164,7 @@ class reminder(commands.Cog):
             today_time = [time.replace(hour=a.hour, minute=a.minute, second=0, microsecond=0) for a in strifted]
             today_time = sorted(today_time)
             print(time, today_time)
-            return self.get_index(today_time, day, time)
+            return self.get_index(today_time, day, time, attendee)
 
     @commands.command()
     async def today(self, ctx):
