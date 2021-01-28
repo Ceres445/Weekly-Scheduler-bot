aa109531cc643325a285e61ba83b6931e0cdae9c
Ceres <ddruva.boomer@gmail.com>
2021-01-28 08:13:13 +0530

    try to config

--- cogs/utils/get_html.py
+++ cogs/utils/get_html.py
@@ -4,21 +4,25 @@ from jinja2 import Template
 import imgkit
 from io import BytesIO
 import os
+
 if 'DYNO' in os.environ:
-    print ('loading wkhtmltopdf path on heroku')
+    print('loading wkhtmltopdf path on heroku')
     WKHTMLTOPDF_CMD = subprocess.Popen(
-        ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')], # Note we default to 'wkhtmltopdf' as the binary name
+        ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')],
+        # Note we default to 'wkhtmltopdf' as the binary name
         stdout=subprocess.PIPE).communicate()[0].strip()
 else:
-    print ('loading wkhtmltopdf path on localhost')
+    print('loading wkhtmltopdf path on localhost')
     MYDIR = os.path.dirname(__file__)
     WKHTMLTOPDF_CMD = os.path.join(MYDIR + "/static/executables/bin/", "wkhtmltopdf.exe")
+
 
 def get_string(elements):
     with open('cogs/utils/index.html', 'r') as f:
         string = f.read()
     template = Template(string)
     buffer = BytesIO()
-    imgkit.from_string(template.render(a=elements, r=range(len(elements))), False).save(buffer, "png")
+    config = imgkit.config(wkhtmltopdf=WKHTMLTOPDF_CMD)
+    imgkit.from_string(template.render(a=elements, r=range(len(elements))), False, config=config).save(buffer, "png")
     buffer.seek(0)
     return buffer
