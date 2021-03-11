ec058c37f27960398d8c6d2400f882870cc25eca
Druva <ddruva.boomer@gmail.com>
2021-03-11 19:30:07 +0530

    improved functions

--- cogs/utils/functions.py
+++ cogs/utils/functions.py
@@ -11,9 +11,46 @@ def convert_format(seconds):
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 
-def hour_rounder(t: datetime = datetime.now()):
-    if t.minute < 30:
-        return (t.replace(second=0, microsecond=0, minute=30, hour=t.hour) - t).total_seconds()
+def hour_rounder(t: datetime = datetime.now(), day: bool = False):
+    if day:
+        return (t.replace(second=0, microsecond=0, minute=0, hour=0) + timedelta(days=1) - t).total_seconds()
     else:
-        return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
-                + timedelta(hours=1) - t).total_seconds()
+        if t.minute < 30:
+            return (t.replace(second=0, microsecond=0, minute=30, hour=t.hour) - t).total_seconds()
+        else:
+            return (t.replace(second=0, microsecond=0, minute=0, hour=t.hour)
+                    + timedelta(hours=1) - t).total_seconds()
+
+
+def get_pings(attendees: str):
+    if attendees == 'int':
+        return f"<@&815982951946125363>"
+    elif attendees == 'crp':
+        return f"<@&698791878421774397>"
+    else:
+        return f"<@&815982951946125363>" + f"<@&698791878421774397>"
+
+
+def get_attendee_name(param):
+    if param == 'int':
+        return 'Integrated'
+    elif param == 'crp':
+        return "CRP"
+    else:
+        return param
+
+
+def get_name(a):
+    if a == 1:
+        return 'today'
+    elif a == 2:
+        return 'in five days'
+    else:
+        return 'in two days'
+
+
+def get_days(date: datetime.date):
+    if date == datetime.now().date():
+        return 'today'
+    else:
+        return f'in {(date - datetime.now().date()).days} days'
